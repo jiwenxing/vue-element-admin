b7fcb42e01aaac59f12f95286f3aa240bbbd0f7c
luyanming <luyanming@jd.com>
2019-06-17 15:26:35 +0800

    update

--- src/views/share/index.vue
+++ src/views/share/index.vue
@@ -3,173 +3,75 @@
     <div class="filter-container">
       <el-form :inline="true" :model="listQuery" class="demo-form-inline" label-width="120px">
         <el-form-item label="Audit Status">
-          <el-select v-model="listQuery.auditStatus" placeholder="audit status" clearable style="width: 150px" class="filter-item bottom-space">
+          <el-select v-model="listQuery.auditStatus" placeholder="audit status" clearable style="width: 150px" class="filter-item">
             <el-option v-for="item in auditStatusOptions" :key="item.key" :label="item.display_name" :value="item.key" />
           </el-select>
         </el-form-item>
         <el-form-item label="User Account">
-          <el-input v-model="listQuery.pin" placeholder="pin" style="width: 150px" class="filter-item bottom-space" @keyup.enter.native="handleFilter" />
+          <el-input v-model="listQuery.pin" placeholder="pin" style="width: 150px" class="filter-item" @keyup.enter.native="handleFilter" />
         </el-form-item>
         <el-form-item label="SKU">
-          <el-input v-model="listQuery.sku" placeholder="commodity number" style="width: 150px" class="filter-item bottom-space" @keyup.enter.native="handleFilter" />
+          <el-input v-model="listQuery.sku" placeholder="commodity number" style="width: 150px" class="filter-item" @keyup.enter.native="handleFilter" />
         </el-form-item>
         <el-form-item label="Order ID">
-          <el-input v-model="listQuery.orderId" placeholder="order number" style="width: 150px" class="filter-item bottom-space" @keyup.enter.native="handleFilter" />
+          <el-input v-model="listQuery.orderId" placeholder="order number" style="width: 150px" class="filter-item" @keyup.enter.native="handleFilter" />
         </el-form-item>
         <br>
         <el-form-item label="Top Status">
-          <el-select v-model="listQuery.topStatus" placeholder="top status" clearable style="width: 150px" class="filter-item bottom-space">
+          <el-select v-model="listQuery.topStatus" placeholder="top status" clearable style="width: 150px" class="filter-item">
             <el-option v-for="item in topStatusOptions" :key="item.key" :label="item.display_name" :value="item.key" />
           </el-select>
         </el-form-item>
-        <el-form-item label="Share Status">
-          <el-select v-model="listQuery.shareStatus" placeholder="share status" clearable style="width: 150px" class="filter-item bottom-space">
-            <el-option v-for="item in shareStatusOptions" :key="item.key" :label="item.display_name" :value="item.key" />
-          </el-select>
-        </el-form-item>
         <el-form-item label="Grade">
-          <el-select v-model="listQuery.grade" placeholder="grade" clearable style="width: 150px" class="filter-item bottom-space">
+          <el-select v-model="listQuery.grade" placeholder="grade" clearable style="width: 150px" class="filter-item">
             <el-option v-for="item in gradeOptions" :key="item.key" :label="item.display_name" :value="item.key" />
           </el-select>
         </el-form-item>
-        <el-form-item label="Nick Name">
-          <el-input v-model="listQuery.orderId" placeholder="nick name" style="width: 150px" class="filter-item bottom-space" @keyup.enter.native="handleFilter" />
-        </el-form-item>
-        <br>
-
         <el-form-item label="Keyword">
-          <el-input v-model="listQuery.keyword" placeholder="keyword" style="width: 435px" class="filter-item bottom-space" @keyup.enter.native="handleFilter" />
-        </el-form-item>
-        <el-form-item label="Category">
-          <category @cate-change="listQuery.category=$event" />
+          <el-input v-model="listQuery.keyword" placeholder="keyword" style="width: 435px" class="filter-item" @keyup.enter.native="handleFilter" />
         </el-form-item>
         <br>
         <el-form-item label="Comment Time">
           <date-time-picker @time-change="listQuery.timeRange=$event" />
         </el-form-item>
-        <el-button v-waves class="filter-item bottom-space" type="primary" icon="el-icon-search" @click="handleFilter">
+        <div v-if="showAll">
+          <el-form-item label="Nick Name">
+            <el-input v-model="listQuery.orderId" placeholder="nick name" style="width: 150px" class="filter-item" @keyup.enter.native="handleFilter" />
+          </el-form-item>
+          <el-form-item label="Share Status">
+            <el-select v-model="listQuery.shareStatus" placeholder="share status" clearable style="width: 150px" class="filter-item">
+              <el-option v-for="item in shareStatusOptions" :key="item.key" :label="item.display_name" :value="item.key" />
+            </el-select>
+          </el-form-item>
+          <el-form-item label="Category">
+            <category @cate-change="listQuery.category=$event" />
+          </el-form-item>
+        </div>
+        <div class="demo-block-control" style="left: 0px;" @click="showAll = !showAll">
+          <i :class="[showAll ? 'el-icon-caret-top' : 'el-icon-caret-bottom']" />
+          <span v-if="showAll" class="tips">hide extra filter options</span>
+          <span v-else class="tips">show more filter options</span>
+        </div>
+        <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
           Search
         </el-button>
-        <el-button v-waves class="filter-item bottom-space" type="danger" icon="el-icon-delete" @click="resetSearch">
+        <el-button v-waves class="filter-item" type="danger" icon="el-icon-delete" @click="resetSearch">
           Clear
         </el-button>
       </el-form>
     </div>
     <el-divider />
-
-    <!-- <div class="well">
-      <el-row>
-        <el-col :span="22">
-          <el-row>
-            <el-col :span="5">
-              <div class="grid-content">
-                <span>审核状态：</span>
-                <el-tag v-for="item in items" :key="item.label" :type="item.type" effect="dark">{{ item.label }}</el-tag>
-              </div>
-            </el-col>
-            <el-col :span="5"><div class="grid-content"><span>用户账号：</span>1234567890</div></el-col>
-            <el-col :span="5"><div class="grid-content"><span>用户昵称：</span>1223213213</div></el-col>
-            <el-col :span="5"><div class="grid-content"><span>评价时间：</span>2019-06-06 13:12:12</div></el-col>
-            <el-col :span="4"><div class="grid-content"><span>IP地址：</span>12.120.102.102</div></el-col>
-          </el-row>
-          <el-row>
-            <el-col :span="5"><span>订单号：</span>1234567890</el-col>
-            <el-col :span="5">
-                <el-row>
-                  <el-col :span="5"><span>评分：</span></el-col>
-                  <el-col :span="19">
-                      <el-rate v-model="score" disabled show-score text-color="#ff9900"></el-rate>
-                  </el-col>
-                </el-row>
-            </el-col>
-            <el-col :span="5"><span>商品号：</span>1234567890</el-col>
-            <el-col :span="9">
-              <span>商品名称：</span><el-link type="primary" href='http://item.jd.com/18164614013.html' target="_blank">电子琴儿童音乐玩具</el-link>
-            </el-col>
-          </el-row>
-          <el-row>
-            <div class="grid-content"><span>原文评价：</span>1234567890</div>
-          </el-row>
-          <el-row>
-              <span>展示评价：</span>1234**7890
-              <el-button type="text" size="medium" round @click="changeCommentDialogVisible = true">修改</el-button>
-              <el-dialog title="修改评价展示内容" :visible.sync="changeCommentDialogVisible">
-                <el-input type="textarea" :rows="2" placeholder="请输入内容" v-model="textarea"></el-input>
-                <div slot="footer" class="dialog-footer">
-                  <el-button @click="changeCommentDialogVisible = false">取 消</el-button>
-                  <el-button type="primary" @click="changeCommentDialogVisible = false">确 定</el-button>
-                </div>
-              </el-dialog>
-              <el-link type="primary" href='http://item.jd.com/18164614013.html' target="_blank">查看回复</el-link>
-          </el-row>
-          <el-row >
-            <el-col :span="2" v-for ="item in urls" :key="item">
-              <img :src="item" style="width: 100px; height: 100px" title="点击放大" @click="bigImgDialog = true">
-              <i class="el-icon-error" title="点击删除" style="position: relative; bottom:98px; right:10px; width: 10px; height: 10px; color:red" @click="imgDelete()"></i>
-            </el-col>
-            <el-dialog title="图片放大效果图" :visible.sync="bigImgDialog">
-              <template>
-                <el-carousel :interval="5000" :arrow="always" :autoplay="false" :initial-index="2"  type="card">
-                  <el-carousel-item v-for="item in urls" :key="item">
-                    <img title="点击删除" :src="item" class="carousel_image_type"/>
-                  </el-carousel-item>
-                </el-carousel>
-              </template>
-              <span slot="footer" class="dialog-footer">
-                <el-button @click="bigImgDialog = false">取 消</el-button>
-                <el-button type="primary" @click="bigImgDialog = false">确 定</el-button>
-              </span>
-            </el-dialog>
-          </el-row>
-
-            <el-row >
-              <el-col :span="2" v-for ="item in urls" :key="item">
-                <el-popover
-                  placement="top-start"
-                  trigger="hover">
-                  <img :src="item">
-                  <img :src="item" style="width: 100px; height: 100px" slot="reference">
-                </el-popover>
-                <i class="el-icon-error" title="点击删除" style="position: relative; bottom:98px; right:10px; width: 10px; height: 10px; color:red" @click="imgDelete()"></i>
-              </el-col>
-            </el-row>
-        </el-col>
-        <el-col :span="2">
-          <template>
-            <div style="margin-top: 20px">
-              <el-radio class="center-horizontal" v-model="radio2" label="1" border size="medium">通过&nbsp&nbsp&nbsp&nbsp</el-radio>
-            </div>
-            <div style="margin-top: 20px">
-              <el-radio class="center-horizontal" v-model="radio2" label="2" border size="medium">不通过</el-radio>
-            </div>
-            <div style="margin-top: 20px">
-              <el-radio class="center-horizontal" v-model="radio2" label="3" border size="medium">不处理</el-radio>
-            </div>
-          </template>
-        </el-col>
-      </el-row>
-    </div>
-     -->
     <pagination v-show="total>=10" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" style="padding: 20px 0 5px" @pagination="getList" />
-    
     <el-form ref="form" :model="items" label-width="80px">
-      
-      <!-- <div v-for ="item in list" :key="item">
-        <el-form-item label="活动名称">
-          {{item.pin}}
-           <el-input v-model="item.pin" ></el-input>
-        </el-form-item>
-      </div> -->
-
       <div v-for ="item in list" :key="item">
         <div class="well">
           <el-row>
             <el-col :span="22">
-              <el-row>
+              <el-row style="margin-bottom: 5px;">
                 <el-col :span="5">
                   <div class="grid-content">
                     <span>审核状态：</span>
-                    <el-tag :key="items[item.imageAuditStatus].label" :type="items[item.imageAuditStatus].type" effect="dark">{{ items[item.imageAuditStatus].label }}</el-tag>
+                    <el-tag size="small" :key="items[item.imageAuditStatus].label" :type="items[item.imageAuditStatus].type" effect="dark">{{ items[item.imageAuditStatus].label }}</el-tag>
                     <!-- <el-tag v-for="item in items" :key="item.label" :type="item.type" effect="dark">{{ item.label }}</el-tag> -->
                   </div>
                 </el-col>
@@ -178,7 +80,7 @@
                 <el-col :span="5"><div class="grid-content"><span>评价时间：</span>{{item.display_time}}</div></el-col>
                 <el-col :span="4"><div class="grid-content"><span>IP地址：</span>{{item.ip}}</div></el-col>
               </el-row>
-              <el-row>
+              <el-row style="margin-bottom: 17px;">
                 <el-col :span="5"><span>订单号：</span>{{item.orderId}}</el-col>
                 <el-col :span="5">
                     <el-row>
@@ -190,13 +92,13 @@
                 </el-col>
                 <el-col :span="5"><span>商品号：</span>{{item.sku}}</el-col>
                 <el-col :span="9">
-                  <span>商品名称：</span><el-link type="primary" :href="'http://item.jd.com/' + item.sku + '.html'" target="_blank">{{item.title}}</el-link>
+                  <span>商品名称：</span><el-link type="primary" :href="'http://item.jd.com/' + item.sku + '.html'" target="_blank">{{item.commodityName}}</el-link>
                 </el-col>
               </el-row>
-              <el-row>
+              <el-row >
                 <div class="grid-content"><span>原文评价：</span>{{item.content}}</div>
               </el-row>
-              <el-row>
+              <el-row style="margin-bottom: 25px;">
                   <span>展示评价：</span>{{item.content}}
                   <el-button type="text" size="medium" round @click="readCommentContent(item.content),changeCommentDialogVisible = true">修改</el-button>
                   <el-dialog title="修改评价展示内容" :visible.sync="changeCommentDialogVisible">
@@ -223,7 +125,7 @@
             <el-col :span="2">
               <template>
                 <div style="margin-top: 20px">
-                  <el-radio class="center-horizontal" v-model="item.imageAuditStatus" label=1 border size="medium">通过&nbsp&nbsp&nbsp&nbsp</el-radio>
+                  <el-radio class="center-horizontal" v-model="item.imageAuditStatus" label=1 border size="medium">通过</el-radio>
                 </div>
                 <div style="margin-top: 20px">
                   <el-radio class="center-horizontal" v-model="item.imageAuditStatus" label=2 border size="medium">不通过</el-radio>
@@ -425,14 +327,6 @@ export default {
       this.listQuery.keyword = ''
       this.getList()
     },
-    tableRowClassName({ row, rowIndex }) {
-      if (row.score === 1) {
-        return 'danger-row'
-      } else if (row.score < 4) {
-        return 'warning-row'
-      }
-      return ''
-    },
     cancelEdit(row) {
       row.content = row.originalContent
       row.edit = false
@@ -596,58 +490,18 @@ export default {
     right: 15px;
     top: 10px;
   }
-  .bottom-space {
+  /* .el-row {
     margin-bottom: 5px;
-  }
-  /* .demo-table-expand {
-    font-size: 0;
   } */
-  /* .demo-table-expand label {
-    width: 90px;
-    color: #99a9bf;
-  } */
-  .demo-table-expand .el-form-item {
-    margin-right: 0;
-    margin-bottom: 0;
-    display: block;
-  }
-
-  .warning-row {
-    background-color: #fff1de;
-  }
-  .danger-row {
-    background-color: #e2d2d5;
-  }
-  /* .demo-block-control{box-sizing:border-box;background-color:#fff;border-bottom-left-radius:4px;border-bottom-right-radius:4px;text-align:center;margin-top:-1px;color:#d3dce6;cursor:pointer;position:relative}
-  .demo-block-control:hover{color:#409eff;background-color:#f9fafc} */
-  .el-row {
-      margin-bottom: 5px;
-    }
   .el-col {
     border-radius: 4px;
-  }
-  .bg-purple-dark {
-    background: #99a9bf;
-  }
-  .bg-purple {
-    background: #d3dce6;
-  }
-  .bg-purple-light {
-    background: #e5e9f2;
   }
   .grid-content {
     border-radius: 4px;
     min-height: 30px;
     line-height: center;
   }
-  .line-height-content{
-    line-height: center;
-  }
-  .row-bg {
-    padding: 10px 0;
-    background-color: #f9fafc;
-  }
-  .well{
+  .well {
     min-height: 20px;
     padding: 19px;
     margin-bottom: 2px;
@@ -657,38 +511,24 @@ export default {
     -webkit-box-shadow: inset 0 1px 1px rgba(0,0,0,.05);
     box-shadow: inset 0 1px 1px rgba(0,0,0,.05);
   }
-  .center-vertical{
-    position: relative;
-    top:50%;
-    transform:translateY(-50%);
-  }
-  .center-horizontal{
+  .center-horizontal {
     position: relative;
     left:50%;
     transform:translateX(-50%);
   }
-  .el-carousel__item h3 {
-    color: #475669;
-    font-size: 18px;
-    opacity: 0.75;
-    line-height: 300px;
-    margin: 0;
+  .demo-block-control {
+    box-sizing:border-box;
+    background-color:#fff;
+    border-bottom-left-radius:4px;
+    border-bottom-right-radius:4px;
+    text-align:center;
+    margin-top:-1px;
+    color:#d3dce6;
+    cursor:pointer;
+    position:relative
   }
-  .el-carousel__item:nth-child(2n) {
-    background-color: #99a9bf;
+  .demo-block-control:hover {
+    color:#409eff;
+    background-color:#f9fafc
   }
-  .el-carousel__item:nth-child(2n+1) {
-    background-color: #d3dce6;
-  }
-  .bannerImg{
-    width: 100%;
-    height: inherit;
-    min-height: 360px;
-    min-width: 1400px;
-  }
-  .carousel_image_type{
-      width: 100%;
-      height: 100%;
-  }
-
 </style>
